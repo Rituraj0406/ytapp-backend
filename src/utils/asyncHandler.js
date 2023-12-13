48c6f1848a3336f7c1a2a1b6d49dd136a0274299
Rituraj0406 <riturajpiku01@gmail.com>
2023-12-14 01:40:07 +0530

    util files and the common function with best practices

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,22 @@
+// asyncHandler using Promise method
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next))
+        .reject((err) => next(err))
+    }
+}
+
+
+// asyncHandler function using try and catch method
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try{
+//         await fn(req, res, next)
+//     } catch (error) {
+//         res.status(err.code || 500).json({
+//             success: false,
+//             message: err.message
+//         })
+//     }
+// }
+
+export { asyncHandler }
