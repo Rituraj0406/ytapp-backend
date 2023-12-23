90190f0fbc09c647a8b44b0495f2990753d570de
Rituraj0406 <riturajpiku01@gmail.com>
2023-12-24 03:08:17 +0530

    added router setup and controller

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ connectDB()
         throw error
     })
 
-    app.listen(process.env.PORT || 8000, () => {
+    app.listen(process.env.PORT || 8080, () => {
         console.log(`Server is running at ${process.env.PORT}`);
     })
 })
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,6 +1,6 @@
 // asyncHandler using Promise method
 const asyncHandler = (requestHandler) => {
-    (req, res, next) => {
+    return (req, res, next) => {
         Promise.resolve(requestHandler(req, res, next))
         .reject((err) => next(err))
     }
